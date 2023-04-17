28aa0d79bad2df631788144ba23a40b065e56e2e
Staselko <Nataselko2021@mail.ru>
2023-04-17 18:32:11 +0300

    Initial commit

---
+++ src/main/java/com/staselko/registrationByEmail/RegistrationByEmailApplication.java
@@ -0,0 +1,13 @@
+package com.staselko.registrationByEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationByEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationByEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staselko/registrationByEmail/RegistrationByEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staselko.registrationByEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationByEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
